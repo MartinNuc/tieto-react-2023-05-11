05f925f5ca219f766cf67fad59f9d2372cbccd54
MartinNuc <martin.nuc@productboard.com>
2023-05-11 20:39:05 +0200

    reset for counter button

--- src/CounterButton.tsx
+++ src/CounterButton.tsx
@@ -1,13 +1,22 @@
 import { useState } from "react";
 
 export function CounterButton() {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState<number>(0);
 
   function handleButtonClick() {
     setCounter(counter + 1);
   }
 
-  return <button onClick={handleButtonClick}>
-    {counter}
-  </button>
+  function handleReset() {
+    setCounter(0);
+  }
+
+  return <>
+    <button onClick={handleButtonClick}>
+      {counter}
+    </button>
+    <button onClick={handleReset}>
+      reset
+    </button>
+  </>
 }
