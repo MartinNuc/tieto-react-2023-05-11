4d8f0a986e5d9a71f7a0f194e57457645a8de22c
MartinNuc <martin.nuc@productboard.com>
2023-05-12 11:49:56 +0200

    Mouse position

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import './App.css';
 import {Table} from './Table';
 import { CounterButton } from './CounterButton';
 import { NumberGenerator } from './NumberGenerator';
+import { MousePosition } from './MousePosition';
 
 function App() {
   return (<>
+    <MousePosition />
     <NumberGenerator pregeneratedCount={4} />
     <Table columns={4} rows={3} />
     <h1>Hello World</h1>
---
+++ src/MousePosition.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import styles from './MousePosition.module.css';
+
+export const MousePosition = () => {
+  const [mousePosition, setMousePosition] = useState({x: 0, y: 0});
+  const [lastClickPosition, setLastClickPosition] = useState({x: 0, y: 0});
+  
+  useEffect(() => {
+    const onMouseMove = (event: MouseEvent) => {
+      console.log(mousePosition);
+      setMousePosition((value) => ({
+        x: event.clientX,
+        y: event.clientY
+      }));
+    };
+    window.addEventListener('mousemove', onMouseMove);
+    
+    return () => {
+      window.removeEventListener('mousemove', onMouseMove);
+    };
+  }, []);
+  
+  useEffect(() => {
+    const onClick = (event: MouseEvent) => {
+      setLastClickPosition({
+        x: event.clientX,
+        y: event.clientY
+      });
+    };
+    window.addEventListener('click', onClick);
+    
+    return () => {
+      window.removeEventListener('click', onClick);
+    };
+  }, []);
+  
+  const isOnTheLeftOfLastClick = mousePosition.x < lastClickPosition.x;
+
+  return (
+    <>
+      <div className={isOnTheLeftOfLastClick ? styles.red : styles.green}>
+        My mouse x position is {mousePosition.x} and y position is {mousePosition.y}
+      </div>
+      <div>
+        Last click: x={lastClickPosition.x}, y={lastClickPosition.y}
+      </div>
+    </>
+  );
+};
